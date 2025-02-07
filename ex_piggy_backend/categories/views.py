6b28a40fd225d2992dabf421172794cc4f41ce8c
rtiwari13 <ritanshutiwari593@gmail.com>
2025-02-07 11:02:21 +0530

    cat api

--- ex_piggy_backend/categories/views.py
+++ ex_piggy_backend/categories/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Category
+from .serializers import CategorySerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class CategoryListCreateView(generics.ListCreateAPIView):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
