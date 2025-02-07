c49098881f62e1ac2aa539ae059127a7e34997ca
rtiwari13 <ritanshutiwari593@gmail.com>
2025-02-07 10:39:28 +0530

    subs api

--- ex_piggy_backend/subscriptions/views.py
+++ ex_piggy_backend/subscriptions/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Subscription
+from .serializers import SubscriptionSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class SubscriptionListCreateView(generics.ListCreateAPIView):
+    serializer_class = SubscriptionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Subscription.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class SubscriptionDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = SubscriptionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Subscription.objects.filter(user=self.request.user)
